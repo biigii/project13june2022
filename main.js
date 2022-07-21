3d31f4e7a0e6807d2c97c260c455a6a6934bd4ee
behshad <82580731+biigii@users.noreply.github.com>
2022-07-21 21:01:24 +1200

    javascript event listeners and functions

--- main.js
+++ main.js
@@ -1,53 +1,30 @@
-//document.getElementById('heading').style.color = "rgb(120, 200, 90)";
-//document.getElementById('heading').style.backgroundColor = '#ff0';
-
-document.querySelector('#heading').style.color = '#00ff00';
-document.querySelector('.heading').style.backgroundColor = '#0000ff';
-//document.querySelector('h1').style.color = '#099'
-//document.querySelector('[data-h1]').style.color = 'rgb(20, 100, 10)';
-//document.querySelector('body').style.backgroundColor = '#f11';
-// document.body.style.backgroundColor = '#f11';
-
-document.querySelector('p').classList.add('my-new-class');
-document.querySelector('body').classList = 'my-custom-body-class';
-document.querySelector('p').setAttribute('id', 'my-custom-id');
-document.querySelector('[data-image]').setAttribute('alt', 'my random sample image');
-document.querySelector('[data-h1]').style.setProperty('color', '#f66');
-
-// var name;
-// const tel = 021123456;
-// let country = 'new zealand';
-
-const h4 = document.createElement('h4');
-h4.innerHTML = 'hello world!';
-document.body.appendChild(h4);
-
-const p = document.createElement('p');
-p.textContent = 'hello world again!';
-p.style.color = '#f11';
-p.classList.add('some-class');
-const section = document.querySelector('#text');
-section.appendChild(p);
-
-function myFirstFunction() {
-  document.body.style.backgroundColor = 'red';
+// adding a new image when button is clicked
+function addImageToBody() {
+  const img = document.createElement('img');
+  img.src = 'https://picsum.photos/id/1061/300';
+  img.alt = 'new image';
+  document.body.appendChild(img);
 }
 
-// myFirstFunction()
+document.querySelector('[data-btn]').addEventListener('click', addImageToBody);
 
-
-/* this function creates a 
-new image and 
-attaches it to the figure element */
-function createNewImage() {
-  const image = document.createElement('img');
-  // image.setAttribute('src', 'https://picsum.photos/id/111/200');
-  image.src = 'https://picsum.photos/id/111/200';
-  image.width = 180;
-  image.alt = 'random photo from picsum photos';
-  image.classList = 'image-class';
-  const imageHolder = document.querySelector('[data-fig]');
-  imageHolder.appendChild(image);
+function changeBodyBackgroundColor() {
+  document.body.style.backgroundColor = '#123';
 }
 
-createNewImage()
+document.querySelector('#button').addEventListener('click', changeBodyBackgroundColor);
+
+function toggleBackgroundColor() {
+  document.querySelector('body').classList.toggle('toggle-background');
+}
+
+document.querySelector('#toggle').addEventListener('click', toggleBackgroundColor);
+
+document.querySelector('[data-another-btn]').addEventListener('click', function () {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'homepage';
+  // h1.innerHTML = 'homepage';
+  h1.style.textTransform = 'capitalize';
+  h1.style.textAlign = 'center';
+  document.body.appendChild(h1);
+});
